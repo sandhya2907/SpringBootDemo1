d7c5d2c141339af6ae6b5a2b32379896e446487b
sanjh-dev <sanjhjaiswal2907>
2020-09-05 12:38:44 +0530

    KIT updated

--- src/main/java/com/example/demo/IDemoRepository.java
+++ src/main/java/com/example/demo/IDemoRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.DemoModel;
 
 public interface IDemoRepository extends JpaRepository<DemoModel, Integer>{
+	//changes for KIT-786
+Optional<DemoModel>findByEmail(String email);
 
 }
